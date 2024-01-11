eb2f694dde20493cfea258e091adce03e7e74439
Arkist <masteroffwuju@gmail.com>
2024-01-11 11:59:22 +0100

    waa waa

--- discord_bot.py
+++ discord_bot.py
@@ -10,6 +10,9 @@ dotenv.load_dotenv()
 intents = discord.Intents(messages=True, guilds=True, message_content=True)
 client = discord.Client(intents=intents)
 
+process_count = -1
+processes = {}
+
 
 @client.event
 async def on_ready():
@@ -18,12 +21,35 @@ async def on_ready():
 
 @client.event
 async def on_message(msg):
+    global processes
+
     if msg.author == client.user:
         return
 
-    if "start" or "track" in msg.content:
-        async for user_msg in await_buster():
-            await msg.reply(user_msg)
+    if "start" in msg.content or "track" in msg.content:
+        process = get_process_id()
+        processes[process] = True
+        print(f"starting process {process}")
+
+        async for user_msg, verbosity in await_buster(processes, process, timeout=120):
+            await msg.reply(user_msg, mention_author=verbosity)
+
+        processes[process] = False
+        print(f"stopped process {process}")
+        return
+
+    elif "stop" in msg.content:
+        for x in processes:
+            processes[x] = False
+        await msg.reply("stopped all processes", mention_author=False)
+        print("stopped all processes")
+
+
+def get_process_id():
+    global process_count
+    process_count += 1
+
+    return process_count
 
 
 client.run(os.environ["TOKEN"])
--- utils.py
+++ utils.py
@@ -2,10 +2,6 @@ import pywinauto
 import pyautogui
 import win32gui
 import asyncio
-import dotenv
-import os
-
-dotenv.load_dotenv()
 
 
 LEAGUE_WINDOW_STR = "League of Legends (TM) Client"
@@ -27,7 +23,10 @@ def get_substring_from_lst(lst, substring):
 
 
 def get_window_with_active(windows):
-    return [w for w in windows if w.is_active()][0]
+    ws = [w for w in windows if w.is_active()]
+    if ws:
+        return ws[0]
+    return None
 
 
 def does_window_exist(window_title):
@@ -40,7 +39,9 @@ def does_window_exist(window_title):
     return any(window_title in title for title in titles)
 
 
-async def await_buster(timeout=20, startup_timeout=20, keepwindowon=False):
+async def await_buster(
+    processes, process, timeout=120, startup_timeout=20, keepwindowon=False
+):
     all_windows = get_all_windows()
 
     league_window = None
@@ -52,21 +53,25 @@ async def await_buster(timeout=20, startup_timeout=20, keepwindowon=False):
         startup_timeout -= 1
 
     if not league_window:
-        yield "LEAGUE WINDOW COULD NOT BE LOCATED"
+        yield "LEAGUE WINDOW COULD NOT BE LOCATED", True
         return
 
-    yield "FOUND WINDOW, STARTING TRACKING"
+    yield "FOUND WINDOW, STARTING TRACKING", False
 
     while 1:
+        if not processes[process]:
+            yield "Stopped Process", False
+            return
         all_windows = get_all_windows()
         last = get_window_with_active(all_windows)
         last_mouse = pyautogui.position()
 
         if not win32gui.GetWindowText(league_window.element_info.handle):
-            yield "GAME ENDED"
+            yield "GAME ENDED", False
             return
 
         if league_window.is_minimized():
+            pyautogui.moveTo(0, 0)
             league_window.restore()
             await asyncio.sleep(0.5)
 
@@ -79,11 +84,11 @@ async def await_buster(timeout=20, startup_timeout=20, keepwindowon=False):
             for y in range(scr.height):
                 cnt += 1 if scr.getpixel((x, y)) == RGB_BUSTER else 0
 
-        print(cnt)
+        # print(cnt)
         if cnt > 10:
-            yield f"POSSIBLE AFK MESSAGE DETECTED WITH {cnt} PIXELS OF COLOUR {RGB_BUSTER}"
+            yield f"POSSIBLE AFK MESSAGE DETECTED WITH {cnt} PIXELS OF COLOUR {RGB_BUSTER}", True
 
-        if last != league_window:
+        if last != league_window and last:
             if not keepwindowon:
                 league_window.minimize()
             last.set_focus()
